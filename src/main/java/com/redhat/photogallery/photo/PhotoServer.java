4b459783cf422a3d226e01e6ae37be36a1a88036
Ales Nosek <anosek@redhat.com>
2019-03-30 23:42:55 -0700

    Use common Vert.x init

--- src/main/java/com/redhat/photogallery/photo/PhotoServer.java
+++ src/main/java/com/redhat/photogallery/photo/PhotoServer.java
@@ -1,17 +1,15 @@
 package com.redhat.photogallery.photo;
 
 import com.redhat.photogallery.common.Server;
-
-import io.vertx.core.logging.SLF4JLogDelegateFactory;
-import io.vertx.reactivex.core.Vertx;
+import com.redhat.photogallery.common.VertxInit;
 
 public class PhotoServer {
 
 	private static final int LISTEN_PORT = 8080;
 
 	public static void main(String[] args) {
-		System.setProperty("vertx.logger-delegate-factory-class-name", SLF4JLogDelegateFactory.class.getName());
-		Vertx vertx = Vertx.vertx();
-		vertx.deployVerticle(new Server(LISTEN_PORT, new PhotoComponent()));
+		VertxInit.createClusteredVertx(vertx -> {
+			vertx.deployVerticle(new Server(LISTEN_PORT, new PhotoComponent()));
+		});
 	}
 }
